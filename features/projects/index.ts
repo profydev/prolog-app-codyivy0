705930ced6edf8e1abb3d8100b32a11b1d67b747
codyivy0 <codyivy0@gmail.com>
2023-12-01 13:02:31 -0600

    error screen and testing

--- features/projects/index.ts
+++ features/projects/index.ts
@@ -1,3 +1,5 @@
 export { useGetProjects } from "./api/use-get-projects";
 export * from "./components/project-list";
 export { default as LoadingSpinner } from "./components/loading-spinner/loading-spinner";
+
+export { default as ErrorMessage } from "./components/error-message/error-message";
