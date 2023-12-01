88989b64fdfaa1ec69d270e32fee1ef4e211905b
codyivy0 <codyivy0@gmail.com>
2023-12-01 15:13:30 -0600

    cypress test loading indicator

--- features/projects/components/loading-spinner/loading-spinner.jsx
+++ features/projects/components/loading-spinner/loading-spinner.jsx
@@ -3,7 +3,7 @@ import styles from "./loading-spinner.module.scss";
 const LoadingSpinner = () => {
   return (
     <div className={styles.loadingContainer}>
-      <div className={styles.ldsring}>
+      <div id="loadingRing" className={styles.ldsring}>
         <div></div>
         <div></div>
         <div></div>
