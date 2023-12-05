31dd5b3266b8f3c227e851b5c4d899a6e84d8f1f
Cody Ivy <110484366+codyivy0@users.noreply.github.com>
2023-12-05 14:01:57 -0600

    UI kit button (#16)
    * Rotate sidebar arrow
    
    * button added and styled/todo icon
    
    * button finished/added to SB

--- features/ui/button/button.tsx
+++ features/ui/button/button.tsx
@@ -1,9 +1,63 @@
-import { ButtonHTMLAttributes } from "react";
 import classNames from "classnames";
 import styles from "./button.module.scss";
 
-export function Button(props: ButtonHTMLAttributes<HTMLButtonElement>) {
+export enum ButtonSize {
+  none = "none",
+  small = "small",
+  medium = "medium",
+  large = "large",
+  xlarge = "xlarge",
+}
+
+export enum ButtonColor {
+  primary = "primary",
+  secondary = "secondary",
+  gray = "gray",
+  empty = "empty",
+  emptyGray = "emptyGray",
+  emptyWhite = "emptyWhite",
+  error = "error",
+}
+
+export enum IconPosition {
+  leading = "leading",
+  trailing = "trailing",
+}
+
+export interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  size?: ButtonSize;
+  color?: ButtonColor;
+  icon?: JSX.Element;
+  iconPosition?: IconPosition;
+}
+
+export function Button({
+  size = ButtonSize.none,
+  color = ButtonColor.emptyWhite,
+  icon,
+  iconPosition,
+  ...props
+}: ButtonProps) {
   return (
-    <button {...props} className={classNames(styles.button, props.className)} />
+    <button
+      {...props}
+      className={classNames(
+        styles.button,
+        styles[size],
+        styles[color],
+        props.className,
+      )}
+    >
+      <span className={styles.buttonContent}>
+        {icon && iconPosition === IconPosition.leading && (
+          <span className={styles.icon}>{icon}</span>
+        )}
+        {props.children}
+        {icon && iconPosition === IconPosition.trailing && (
+          <span className={styles.icon}>{icon}</span>
+        )}
+      </span>
+    </button>
   );
 }
